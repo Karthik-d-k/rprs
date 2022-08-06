c5fe74a3d1c3484cee3f9dda6d185368c972324d
karthikdk <karthikdk1998@gmail.com>
2022-08-06 14:35:20 +0530

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+
+use std::{io, fs, env};
+use std::path::{Path, PathBuf};
+use std::error::Error;
+
+pub struct Config {
+    pub src_dir: PathBuf,
+    pub des_dir: PathBuf,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        let src_dir = std::env::args().nth(1).expect("Source directory is missing");
+        let des_dir = std::env::args().nth(2).expect("Destination directory is missing");
+
+        let src_dir = PathBuf::from(src_dir);
+        let des_dir = PathBuf::from(des_dir);
+
+        if !src_dir.exists() {
+            Err("<src_dir> does not exits!!")
+        } else if !des_dir.exists() {
+            Err("<des_dir> does not exits!!")
+        } else {
+            Ok(Config {
+            src_dir,
+            des_dir,
+        })
+        }
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    for entry in list_files(config.src_dir)? {
+        println!("{:?}", entry);
+    }
+    for entry in list_files(config.des_dir)? {
+        println!("{:?}", entry);
+    }
+
+    Ok(())
+}
+
+fn _list_files(vec: &mut Vec<PathBuf>, path: PathBuf) -> io::Result<()> {
+    if path.is_dir() {
+        let paths = fs::read_dir(&path)?;
+        for path_result in paths {
+            let full_path = path_result?.path();
+            _list_files(vec, full_path);
+        }
+    } else {
+        vec.push(path);
+    }
+    Ok(())
+}
+
+fn list_files<T: Into<PathBuf>>(path: T) -> io::Result<Vec<PathBuf>> {
+    let mut vec = Vec::new();
+    let path = path.into();
+    _list_files(&mut vec, path);
+    Ok(vec)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process};
+use rprs::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        eprintln!("Usage: <rprs> <src_dir> <des_dir>");
+
+        process::exit(1);
+    });
+
+    if let Err(e) = rprs::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
